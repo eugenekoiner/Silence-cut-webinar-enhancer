8a2aa1423cfd48ccf03ebf16d8cda66967b2e3e0
eugenekoiner <eugenekoiner@gmail.com>
2024-11-09 01:07:34 +0700

    удалил мусор

--- settings.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-# Проверка доступности GPU
-def get_device():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Используемое устройство: {device}")
-    return device
